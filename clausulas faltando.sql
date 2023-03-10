e930a2f6463f3b331608d4d0ba744617aa42d4ac
Eduardo Amorim <72039442+Edu-p@users.noreply.github.com>
2023-03-09 22:04:14 -0300

    add: more queries

--- clausulas faltando.sql
+++ clausulas faltando.sql
@@ -40,4 +40,51 @@ UNION
 SELECT u.*, c.* 
 FROM usuario u
 
+-- Selecionar todos os cursos que possuem aulas com duração maior que a média de duração das aulas de todos os cursos publicados.
+SELECT c.Tema/Título
+FROM Curso c
+WHERE EXISTS (
+  SELECT *
+  FROM Aula a
+  WHERE a.ID_Curso = c.ID
+  AND a.Duração > (
+    SELECT AVG(a2.Duração)
+    FROM Aula a2
+    WHERE a2.ID_Curso = c.ID
+  )
+)
+
+-- Selecionar todos os cursos que foram comprados por alunos que possuem o CPF "123.456.789-10".
+SELECT c.titulo
+FROM Curso c
+WHERE c.id IN (
+  SELECT ec.id_urso
+  FROM Efetuar_Compra ec
+  INNER JOIN Aluno a ON a.CPF = ec.cpf_aluno
+  WHERE a.cpf = '23975024887'
+)
+
+-- Listar os cursos com valor superior ao valor médio de todos os cursos publicados por um determinado educador.
+SELECT c.titulo, c.valor
+FROM curso c
+WHERE c.valor > ANY (
+   SELECT AVG(valor) 
+   FROM curso 
+   WHERE id_educador = '41653193506'
+);
+
+-- Projetar titulos das aulas com maior duração que as aulas do curso 'Direito Trabalhista'
+SELECT titulo 
+FROM Aula
+WHERE duracao > ALL
+        (
+            SELECT duracao
+            FROM Aula a INNER JOIN 
+                Curso c a.id_curso = c.id
+            WHERE c.titulo = 'Direito Trabalhista'
+        )
+
+
+
+
 
