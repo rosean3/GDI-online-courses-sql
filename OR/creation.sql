f20045e685d2b01b69b6b3dca0cef88e5e361f43
Eduardo Amorim <72039442+Edu-p@users.noreply.github.com>
2023-03-30 18:01:12 -0300

    add: creation of some entities

---
+++ OR/creation.sql
@@ -0,0 +1,36 @@
+create or replace type tp_curso as object(
+	titulo varchar2(30),
+    valor number,
+    descricao varchar(50),
+    duracao number,
+    id number
+);
+
+create or replace type tp_usuario_endereco as object(
+	complemento varchar2(30),
+	cep varchar(50),
+    numero number
+);
+ 
+CREATE TYPE tp_usuario_fone AS OBJECT(
+	numero varchar2(30)
+);
+
+CREATE TYPE varray_fone AS VARRAY (5) OF tp_usuario_fone;
+
+create or replace type tp_usuario as object(
+	nome varchar2(30),
+    cpf varchar2(20),
+    endereco tp_usuario_endereco,
+    duracao number,
+    lista_fones varray_fone
+)not final not INSTANTIABLE;
+
+create or replace type tp_administrador under tp_usuario(  
+);
+
+create or replace type tp_educador under tp_usuario(  
+);
+
+create or replace type tp_aluno under tp_usuario(  
+);
