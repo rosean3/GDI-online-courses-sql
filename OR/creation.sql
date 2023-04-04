dffcf63a77cdf0ad9e4e111fa61532212a92a19a
Roseane Gama <roseanecavalcantee@hotmail.com>
2023-04-04 12:12:13 -0300

    first commit

--- OR/creation.sql
+++ OR/creation.sql
@@ -1,3 +1,53 @@
+create or replace type tp_usuario_fone AS OBJECT(
+	numero char(11)
+);
+/
+
+create or replace type tp_usuario_fones AS VARRAY (5) OF tp_usuario_fone;
+/
+
+create or replace type tp_usuario_endereco as object(
+	complemento varchar2(30),
+	cep char(8),
+    numero int
+);
+/
+
+create or replace type tp_usuario as object(
+    cpf char(11),
+	nome varchar2(30),
+    endereco tp_usuario_endereco,
+    lista_fones tp_usuario_fones
+) not final not INSTANTIABLE;
+/
+
+create or replace type tp_educador under tp_usuario( 
+);
+/
+
+create or replace type tp_aluno under tp_usuario(  
+);
+/
+
+create or replace type tp_administrador under tp_usuario(
+);
+/
+
+CREATE TABLE tb_educador of tp_educador(
+    cpf PRIMARY KEY
+);
+/
+
+CREATE TABLE tb_aluno of tp_aluno(
+    cpf PRIMARY KEY
+);
+/
+
+CREATE TABLE tb_administrador of tp_administrador(
+    cpf PRIMARY KEY
+);
+/
+ 
 create or replace type tp_aula as object(
 	num_da_aula int,
     titulo varchar2(300),
@@ -13,86 +63,67 @@ create or replace type tp_curso as object(
 	titulo varchar2(300),
     valor float,
     descricao varchar(4000),
-    data_hora_publicacao varchar2(20),
-    lista_aulas tp_lista_aulas
-);
-/
-
--- CREATE TABLE tb_curso OF tp_curso NESTED TABLE lista_aulas
--- STORE AS nt_aulas;
-
-create or replace type tp_curso_relac as object(
-    curso ref tp_curso
-);
-/
-
-create or replace type tp_nt_cursos as table of tp_curso;
-/
-
-create or replace type tp_usuario_endereco as object(
-	complemento varchar2(30),
-	cep varchar(50),
-    numero number
-);
-/
-
-CREATE TYPE tp_usuario_fone AS OBJECT(
-	numero varchar2(30)
-);
-/
-
-CREATE TYPE varray_fone AS VARRAY (5) OF tp_usuario_fone;
-/
-
-create or replace type tp_usuario as object(
-	nome varchar2(30),
-    cpf varchar2(20),
-    endereco tp_usuario_endereco,
-    duracao number,
-    lista_fones varray_fone
-)not final not INSTANTIABLE;
-/
-
-create or replace type tp_educador under tp_usuario( 
-    lista_cursos tp_nt_cursos 
+    lista_aulas tp_lista_aulas,
+    data_hora_publicacao varchar2(20), -- relacionamento publicar
+    educador ref tp_educador, -- relacionamento publicar
+    administrador ref tp_administrador -- relacionamento publicar
 );
 /
     
-create or replace type tp_educador_curso_relac as object(
-    lista_educadores ref tp_educador 
-);
-/
-
-create or replace type tp_nt_educador_curso as table of tp_educador_curso_relac;
-/
-
-create or replace type tp_administrador under tp_usuario(
-    lista_cursos_educadores tp_nt_educador_curso
-);
-/
-
-create or replace type tp_aluno under tp_usuario(  
-    lista_aulas_assistir tp_nt_aula
-);
-/
+CREATE TABLE tb_curso OF tp_curso(
+    id PRIMARY KEY
+) NESTED TABLE lista_aulas
+STORE AS nt_aulas;
 
 create or replace type tp_efetuar_compra as object(
-    aluno ref tp_aluno,
-    curso ref tp_curso,
-    data_hora_compra varchar2(30)
+    id_ec int,
+    curso_ec ref tp_curso,
+    aluno_ec ref tp_aluno,
+    data_hora_compra timestamp
 );
 /
 
+DROP TABLE tb_efetuar_compra;
+/
+
+CREATE TABLE tb_efetuar_compra of tp_efetuar_compra(
+	id_ec PRIMARY KEY,
+    aluno_ec WITH ROWID REFERENCES tb_aluno,
+	curso_ec WITH ROWID REFERENCES tb_curso
+);
+/
+
+    
 create or replace type tp_assistir as object(
-    aula ref tp_aula,
-    aluno ref tp_aluno,
-    data_hora_assitida varchar2(30)
+	id_as INT,
+    aula_as ref tp_aula,
+    aluno_as ref tp_aluno,
+    data_hora_assitir timestamp
+);
+/
+
+DROP TABLE tb_assistir;
+
+CREATE TABLE tb_assistir of tp_assistir(
+	id_as PRIMARY KEY,
+	aluno_as WITH ROWID REFERENCES tb_aluno
+    -- aula_as WITH ROWID REFERENCES tp_lista_aulas
+    -- tentar referenciar aula de algum jeito
 );
 /
 
 create or replace type tp_presentear_curso as object(
+    id_pc INT,
     aluno_presenteado ref tp_aluno,
     aluno_presenteador ref tp_aluno,
     curso ref tp_curso
+);
+/
+
+CREATE TABLE tb_presentear_curso of tp_presentear_curso(
+	id_pc PRIMARY KEY,
+	aluno_presenteado WITH ROWID REFERENCES tb_aluno,
+    aluno_presenteador WITH ROWID REFERENCES tb_aluno,
+	curso WITH ROWID REFERENCES tb_curso
 );
 /
