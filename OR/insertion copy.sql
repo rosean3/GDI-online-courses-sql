b5d42b45b5cf3918fc3e7a0010f213d39e5ca99c
Roseane Gama <roseanecavalcantee@hotmail.com>
2023-04-04 23:18:22 -0300

    feat: changes in insertion

--- OR/insertion copy.sql
+++ OR/insertion copy.sql
@@ -1,21 +1,34 @@
---inserindo em tb_educador
+--inserindo em tb_aluno
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('01731334917', 'Josué', tp_usuario_endereco('Bloco A', '00000000', 42), tp_usuario_fones(tp_usuario_fone('11111111111')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('13451468741', 'Mario', tp_usuario_endereco('Bloco C', '85370453', 42), tp_usuario_fones(tp_usuario_fone('49473798353')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('00000000001', 'Ruy', tp_usuario_endereco('Apartamento 101', '11111111', 12), tp_usuario_fones(tp_usuario_fone('22222222222')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('26419720291', 'Rui', tp_usuario_endereco('Apartamento 202', '85370453', 4), tp_usuario_fones(tp_usuario_fone('74554869342')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('06392252608', 'Anjolina', tp_usuario_endereco('NULL', '22222222', 13), tp_usuario_fones(tp_usuario_fone('33333333333')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('21475327332', 'Leticia', tp_usuario_endereco(NULL, '85246150', 13), tp_usuario_fones(tp_usuario_fone('88758595463'), tp_usuario_fone('67710687600')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('42750271500', 'Guilherme', tp_usuario_endereco('Bloco 3', '33333333', 12), tp_usuario_fones(tp_usuario_fone('44444444444')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('87829442544', 'Rafael', tp_usuario_endereco('Bloco 3', '38489172', 12), tp_usuario_fones(tp_usuario_fone('83943873983')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('42993006152', 'Silvio', tp_usuario_endereco('Apartamento 302', '44444444', 4), tp_usuario_fones(tp_usuario_fone('55555555555')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('86291960821', 'Maria', tp_usuario_endereco('Apartamento 402', '11196898', 4), tp_usuario_fones(tp_usuario_fone('28449310507')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('72048996937', 'Gustavo', tp_usuario_endereco('Bloco C', '55555555', 1), tp_usuario_fones(tp_usuario_fone('66666666666')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('32274181942', 'Katia', tp_usuario_endereco('Bloco C', '84513522', 1), tp_usuario_fones(tp_usuario_fone('60903915891')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('90399870064', 'Luis', tp_usuario_endereco(NULL, '44444444', 4), tp_usuario_fones(tp_usuario_fone('55555555555')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('66402019823', 'Barbara', tp_usuario_endereco(NULL, '46407908', 4), tp_usuario_fones(tp_usuario_fone('19072354622'), tp_usuario_fone('92914517633')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('18409941996', 'Marcelo', tp_usuario_endereco('1104', '55555555', 1), tp_usuario_fones(tp_usuario_fone('66666666666')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('10918637289', 'Marcelo', tp_usuario_endereco('1104', '23745093', 1), tp_usuario_fones(tp_usuario_fone('57429170002'), tp_usuario_fone('73117579487')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('77447413012', 'Fernando', tp_usuario_endereco(NULL, '66666666', NULL), tp_usuario_fones(tp_usuario_fone('77777777777')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('87387698251', 'Ernesto', tp_usuario_endereco(NULL, '88379717', NULL), tp_usuario_fones(tp_usuario_fone('57625705738'), tp_usuario_fone('26409745131')));
 
-INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('23268225852', 'Beatriz', tp_usuario_endereco(NULL, '77777777', NULL), tp_usuario_fones(tp_usuario_fone('88888888888')));
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('86298332544', 'Beatrice', tp_usuario_endereco(NULL, '87801111', NULL), tp_usuario_fones(tp_usuario_fone('32717751523')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('11936476244', 'Robson', tp_usuario_endereco('Apartamento 1104', '21791174', 1), tp_usuario_fones(tp_usuario_fone('51485148493')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('73964907123', 'Alice', tp_usuario_endereco(NULL, '16655300', 13), tp_usuario_fones(tp_usuario_fone('33317203735')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('47680877364', 'Matheus', tp_usuario_endereco('Bloco 2', '10562321', 12), tp_usuario_fones(tp_usuario_fone('98451784511')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('66046090696', 'Gabriel', tp_usuario_endereco('Apartamento 602', '11387378', 4), tp_usuario_fones(tp_usuario_fone('69692040093')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('81206614383', 'Eulalia', tp_usuario_endereco('Bloco E', '12272583', 1), tp_usuario_fones(tp_usuario_fone('52831167612'), tp_usuario_fone('73388513183'), tp_usuario_fone('84725873884')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('43846654031', 'Luiz', tp_usuario_endereco(NULL, '88054179', 4), tp_usuario_fones(tp_usuario_fone('5604356058'), tp_usuario_fone('85428572608'), tp_usuario_fone('36591697904')));
+
--- OR/insertion.sql
+++ OR/insertion.sql
@@ -19,3 +19,38 @@ INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('18409941996'
 INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('77447413012', 'Fernando', tp_usuario_endereco(NULL, '73461926', NULL), tp_usuario_fones(tp_usuario_fone('65739301198'), tp_usuario_fone('56385266170')));
 
 INSERT INTO tb_educador (cpf, nome, endereco, lista_fones) VALUES ('23268225852', 'Beatriz', tp_usuario_endereco(NULL, '36826442', NULL), tp_usuario_fones(tp_usuario_fone('88888888888'), tp_usuario_fone('60351492835'), tp_usuario_fone('82754305473')));
+
+--inserindo em tb_aluno
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('13451468741', 'Mario', tp_usuario_endereco('Bloco C', '85370453', 42), tp_usuario_fones(tp_usuario_fone('49473798353')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('26419720291', 'Rui', tp_usuario_endereco('Apartamento 202', '85370453', 4), tp_usuario_fones(tp_usuario_fone('74554869342')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('21475327332', 'Leticia', tp_usuario_endereco(NULL, '85246150', 13), tp_usuario_fones(tp_usuario_fone('88758595463'), tp_usuario_fone('67710687600')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('87829442544', 'Rafael', tp_usuario_endereco('Bloco 3', '38489172', 12), tp_usuario_fones(tp_usuario_fone('83943873983')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('86291960821', 'Maria', tp_usuario_endereco('Apartamento 402', '11196898', 4), tp_usuario_fones(tp_usuario_fone('28449310507')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('32274181942', 'Katia', tp_usuario_endereco('Bloco C', '84513522', 1), tp_usuario_fones(tp_usuario_fone('60903915891')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('66402019823', 'Barbara', tp_usuario_endereco(NULL, '46407908', 4), tp_usuario_fones(tp_usuario_fone('19072354622'), tp_usuario_fone('92914517633')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('10918637289', 'Marcelo', tp_usuario_endereco('1104', '23745093', 1), tp_usuario_fones(tp_usuario_fone('57429170002'), tp_usuario_fone('73117579487')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('87387698251', 'Ernesto', tp_usuario_endereco(NULL, '88379717', NULL), tp_usuario_fones(tp_usuario_fone('57625705738'), tp_usuario_fone('26409745131')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('86298332544', 'Beatrice', tp_usuario_endereco(NULL, '87801111', NULL), tp_usuario_fones(tp_usuario_fone('32717751523')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('11936476244', 'Robson', tp_usuario_endereco('Apartamento 1104', '21791174', 1), tp_usuario_fones(tp_usuario_fone('51485148493')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('73964907123', 'Alice', tp_usuario_endereco(NULL, '16655300', 13), tp_usuario_fones(tp_usuario_fone('33317203735')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('47680877364', 'Matheus', tp_usuario_endereco('Bloco 2', '10562321', 12), tp_usuario_fones(tp_usuario_fone('98451784511')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('66046090696', 'Gabriel', tp_usuario_endereco('Apartamento 602', '11387378', 4), tp_usuario_fones(tp_usuario_fone('69692040093')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('81206614383', 'Eulalia', tp_usuario_endereco('Bloco E', '12272583', 1), tp_usuario_fones(tp_usuario_fone('52831167612'), tp_usuario_fone('73388513183'), tp_usuario_fone('84725873884')));
+
+INSERT INTO tb_aluno (cpf, nome, endereco, lista_fones) VALUES ('43846654031', 'Luiz', tp_usuario_endereco(NULL, '88054179', 4), tp_usuario_fones(tp_usuario_fone('5604356058'), tp_usuario_fone('85428572608'), tp_usuario_fone('36591697904')));
+
